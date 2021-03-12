0692a9f6e97e891470bddd0d2a768e76191388b2
y-du <42994541+y-du@users.noreply.github.com>
2021-03-12 16:36:42 +0100

    add and update endpoint handlers

--- trainer/api.py
+++ trainer/api.py
@@ -19,7 +19,9 @@ __all__ = ("Model", )
 
 from .logger import getLogger
 from . import handlers
+from . import model
 import falcon
+import json
 
 
 logger = getLogger(__name__.split(".", 1)[-1])
@@ -33,15 +35,48 @@ def reqErrorLog(req, ex):
     logger.error("method='{}' path='{}' - {}".format(req.method, req.path, ex))
 
 
+class Models:
+    def __init__(self, stg_handler: handlers.Storage, conf_handler: handlers.Configs, jobs_handler: handlers.Jobs):
+        self.__stg_handler = stg_handler
+        self.__conf_handler = conf_handler
+        self.__jobs_handler = jobs_handler
+
+    def on_post(self, req: falcon.request.Request, resp: falcon.response.Response):
+        reqDebugLog(req)
+        try:
+            data = json.load(req.bounded_stream)
+            config_model_id_list = self.__conf_handler.get_config_model_id_list(
+                service_id=data[model.ModelRequest.service_id],
+                config=data[model.ModelRequest.ml_config]
+            )
+            for item in config_model_id_list:
+                try:
+                    self.__stg_handler.get(b"models-", item[model.ModelResponse.model_id].encode())
+                except KeyError:
+                    self.__jobs_handler.create(
+                        {
+                            model.Job.service_id: data[model.ModelRequest.service_id],
+                            model.Job.model_id: item[model.ModelResponse.model_id],
+                            model.Job.config: item[model.ModelResponse.config],
+                        }
+                    )
+            resp.content_type = falcon.MEDIA_JSON
+            resp.body = json.dumps(config_model_id_list)
+            resp.status = falcon.HTTP_200
+        except Exception as ex:
+            resp.status = falcon.HTTP_500
+            reqErrorLog(req, ex)
+
+
 class Model:
     def __init__(self, stg_handler: handlers.Storage):
         self.__stg_handler = stg_handler
 
-    def on_get(self, req: falcon.request.Request, resp: falcon.response.Response, service_id: str):
+    def on_get(self, req: falcon.request.Request, resp: falcon.response.Response, model_id: str):
         reqDebugLog(req)
         try:
             resp.content_type = falcon.MEDIA_JSON
-            resp.body = self.__stg_handler.get(service_id.encode())
+            resp.body = self.__stg_handler.get(b"models-", model_id.encode())
             resp.status = falcon.HTTP_200
         except KeyError as ex:
             resp.status = falcon.HTTP_404
@@ -50,20 +85,56 @@ class Model:
             resp.status = falcon.HTTP_500
             reqErrorLog(req, ex)
 
-    def on_put(self, req: falcon.request.Request, resp: falcon.response.Response, service_id: str):
+    # def on_put(self, req: falcon.request.Request, resp: falcon.response.Response, service_id: str):
+    #     reqDebugLog(req)
+    #     try:
+    #         self.__stg_handler.put(service_id.encode(), req.bounded_stream.read())
+    #         resp.status = falcon.HTTP_200
+    #     except Exception as ex:
+    #         resp.status = falcon.HTTP_500
+    #         reqErrorLog(req, ex)
+
+    # def on_delete(self, req: falcon.request.Request, resp: falcon.response.Response, service_id: str):
+    #     reqDebugLog(req)
+    #     try:
+    #         self.__stg_handler.delete(service_id.encode())
+    #         resp.status = falcon.HTTP_200
+    #     except Exception as ex:
+    #         resp.status = falcon.HTTP_500
+    #         reqErrorLog(req, ex)
+
+
+class Jobs:
+    def __init__(self, jobs_handler: handlers.Jobs):
+        self.__jobs_handler = jobs_handler
+
+    def on_get(self, req: falcon.request.Request, resp: falcon.response.Response):
         reqDebugLog(req)
         try:
-            self.__stg_handler.put(service_id.encode(), req.bounded_stream.read())
+            resp.content_type = falcon.MEDIA_JSON
+            resp.body = json.dumps(self.__jobs_handler.list_jobs())
             resp.status = falcon.HTTP_200
+        except KeyError as ex:
+            resp.status = falcon.HTTP_404
+            reqErrorLog(req, ex)
         except Exception as ex:
             resp.status = falcon.HTTP_500
             reqErrorLog(req, ex)
 
-    def on_delete(self, req: falcon.request.Request, resp: falcon.response.Response, service_id: str):
+
+class Job:
+    def __init__(self, jobs_handler: handlers.Jobs):
+        self.__jobs_handler = jobs_handler
+
+    def on_get(self, req: falcon.request.Request, resp: falcon.response.Response, job_id):
         reqDebugLog(req)
         try:
-            self.__stg_handler.delete(service_id.encode())
+            resp.content_type = falcon.MEDIA_JSON
+            resp.body = json.dumps(self.__jobs_handler.get_job(job_id))
             resp.status = falcon.HTTP_200
+        except KeyError as ex:
+            resp.status = falcon.HTTP_404
+            reqErrorLog(req, ex)
         except Exception as ex:
             resp.status = falcon.HTTP_500
             reqErrorLog(req, ex)
